84717c0b237640cf86a9b746812bea88f5c38960
martinlee159 <martinlee159@163.com>
2018-07-27 15:16:19 +0800

    Modified example/exchange
    Add send bu demo

--- example/exchange.js
+++ example/exchange.js
@@ -2,6 +2,7 @@
 
 require('chai').should();
 const BumoSDK = require('../index');
+const BigNumber = require('bignumber.js');
 
 const sdk = new BumoSDK({
   host: 'seed1.bumotest.io:26002',
@@ -84,7 +85,6 @@ describe('The demo of bumo-sdk for exchange ', function() {
     console.log(data);
   });
 
-
   it('Get fees in the specified blockNumber', async() => {
     const data = await sdk.block.getFees('100');
     console.log(data);
@@ -94,5 +94,90 @@ describe('The demo of bumo-sdk for exchange ', function() {
     const data = await sdk.block.getLatestFees();
     console.log(data);
   });
-  
+
+  // ====================================
+  // Send BU contains 4 steps:
+  // 1. build operation (buSendOperation)
+  // 2. build blob
+  // 3. sign blob with sender private key
+  // 4. submit transaction
+  // ====================================
+  it('Send BU', async() => {
+    const senderPrivateKey = 'sender private key';
+    const senderAddress = 'buQavuuHbqQz1Uc7kpY9zWLGup9GuoBLd5g8';
+    const receiverAddress = 'buQsBMbFNH3NRJBbFRCPWDzjx7RqRc1hhvn1';
+
+    const accountInfo = await sdk.account.getNonce(senderAddress);
+
+    if (accountInfo.errorCode !== 0) {
+      console.log(accountInfo);
+      return;
+    }
+    let nonce = accountInfo.result.nonce;
+    // nonce + 1
+    nonce = new BigNumber(nonce).plus(1).toString(10);
+
+    // ====================================
+    // 1. build operation (buSendOperation)
+    // ====================================
+    const operationInfo = sdk.operation.buSendOperation({
+      sourceAddress: senderAddress,
+      destAddress: receiverAddress,
+      buAmount: '7000',
+      metadata: 'send bu demo',
+    });
+
+    if (operationInfo.errorCode !== 0) {
+      console.log(operationInfo);
+      return;
+    }
+
+    const operationItem = operationInfo.result.operation;
+
+    // ====================================
+    // 2. build blob
+    // ====================================
+    const blobInfo = sdk.transaction.buildBlob({
+      sourceAddress: senderAddress,
+      gasPrice: '1000',
+      feeLimit: '306000',
+      nonce,
+      operations: [ operationItem ],
+    });
+
+    if (blobInfo.errorCode !== 0) {
+      console.log(blobInfo);
+      return;
+    }
+
+    const blob = blobInfo.result.transactionBlob;
+
+    // ====================================
+    // 3. sign blob with sender private key
+    // ====================================
+    let signatureInfo = sdk.transaction.sign({
+      privateKeys: [ senderPrivateKey ],
+      blob,
+    });
+
+    if (signatureInfo.errorCode !== 0) {
+      console.log(signatureInfo);
+      return;
+    }
+
+    const signature = signatureInfo.result.signatures;
+
+    // ====================================
+    // 4. submit transaction
+    // ====================================
+    const transactionInfo = await sdk.transaction.submit({
+      blob,
+      signature: signature,
+    });
+
+    if (transactionInfo.errorCode !== 0) {
+      console.log(transactionInfo);
+    }
+    console.log(transactionInfo);
+  });
 });
